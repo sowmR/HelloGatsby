df1636a9cfff12d55d81787a06d766c834eb93da
Sowmya R <sowmyas@baasha.net>
2021-05-11 15:12:31 -0400

    Add files via upload

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+import "./public/css/styles.css";
+import "./node_modules/bootstrap";
+import "./node_modules/jquery";
